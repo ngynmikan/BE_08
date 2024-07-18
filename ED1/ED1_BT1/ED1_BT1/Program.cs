7beca02815eb9ea3a88cffab8db7757440650629
Nguyen Minh Khang <ngynmikan@gmail.com>
2024-07-18 19:53:33 +0700

    Submit BaiTap1

---
+++ ED1/ED1_BT1/ED1_BT1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED1_BT1
+{
+    internal class Program
+    {
+
+        static void Sum(int a, int b)
+        {
+            Console.WriteLine(" Sum {0} and {1} : {2}", a, b, a + b);
+        }
+
+        static void Min(int a, int b)
+        {
+            Console.WriteLine(" Min {0} and {1} : {2}", a, b, a - b);
+        }
+
+        static void Product(int a, int b)
+        {
+            Console.WriteLine(" Product {0} and {1} : {2}", a, b, a * b);
+        }
+
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Input number 1: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input number 2: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Sum(a, b);
+            Min(a, b);
+            Product(a, b);
+
+            Console.ReadKey();
+        }
+    }
+}
