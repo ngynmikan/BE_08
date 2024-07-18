47c24d0ed00aef8b10139a4892724c938b7bc894
Nguyen Minh Khang <ngynmikan@gmail.com>
2024-07-18 20:01:50 +0700

    Submit BaiTap2

---
+++ ED1/ED1_BT2/ED1_BT2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED1_BT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input C degree: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kevin degree = {0}", celsius + 273);
+            Console.WriteLine("Fahrenheit degree = {0}", celsius * 18 / 10 +32);
+            Console.ReadKey();
+        }
+    }
+}
